64ef82a342284a4645e6b30dc465f73cbc98eb55
swarnalee paul <paulswarnalee01@gmail.com>
2025-01-19 11:15:22 +0530

    commited

--- Backend/app.js
+++ Backend/app.js
@@ -8,9 +8,13 @@ const userRoutes=require('./routes/user.routes');
 const captainRoutes = require('./routes/captain.routes');
 const mapRoutes=require("./routes/maps.routes");
 const rideRoutes=require("./routes/ride.routes");
+const path =require('path');
+
+
 
 connectDB()
 const app=express();
+const _dirname=path.resolve()
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cors());
@@ -25,5 +29,13 @@ app.use("/captains",captainRoutes);
 app.use('/maps',mapRoutes)
 app.use("/rides",rideRoutes);
 
+app.use(express.static(path.join(__dirname,"/Frontend/dist")));
+app.get('*',(req,res)=>{
+    res.sendFile(path.resolve(__dirname, "Frontend", "dist", "index.html"));
+}) 
 
+app.use(cors({
+    origin: " http://localhost:5173", 
+    credentials: true
+}));
 module.exports=app;
