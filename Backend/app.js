1bdfb65305c777d3906392ddbf80c79693715529
swarnalee paul <paulswarnalee01@gmail.com>
2025-01-19 11:43:05 +0530

    commited

--- Backend/app.js
+++ Backend/app.js
@@ -20,9 +20,9 @@ app.use(express.urlencoded({extended:true}));
 app.use(cors());
 
 app.use(cookieParser());
-app.get('/', (req, res) => {
-    res.send('Hello World');
-});
+// app.get('/', (req, res) => {
+//     res.send('Hello World');
+// });
 
 app.use('/users',userRoutes);
 app.use("/captains",captainRoutes);
