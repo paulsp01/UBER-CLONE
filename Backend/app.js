89056a462918077680a9cd612b9d4b2ca7300f5c
swarnalee paul <paulswarnalee01@gmail.com>
2025-01-19 21:25:38 +0530

    updated

--- Backend/app.js
+++ Backend/app.js
@@ -1,5 +1,5 @@
 const dotenv=require('dotenv');
-require(dotenv.config());
+dotenv.config();
 const express=require('express');
 const cors=require('cors');
 const cookieParser=require("cookie-parser");
