45495361bf5fd18bc3174c16c2e05aea64b1de68
swarnalee paul <paulswarnalee01@gmail.com>
2025-01-19 20:59:45 +0530

    commited

--- Backend/app.js
+++ Backend/app.js
@@ -1,5 +1,5 @@
 const dotenv=require('dotenv');
-dotenv.config();
+require(dotenv.config());
 const express=require('express');
 const cors=require('cors');
 const cookieParser=require("cookie-parser");
