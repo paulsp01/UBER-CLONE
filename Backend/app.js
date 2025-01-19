aa3c4ae88f32300cb82db0da52cfb2ff90e47dbf
swarnalee paul <paulswarnalee01@gmail.com>
2025-01-19 20:25:58 +0530

    commited

--- Backend/app.js
+++ Backend/app.js
@@ -33,6 +33,10 @@ app.use(express.static(path.join(__dirname,'public')));
 app.get('*',(req,res)=>{
     res.sendFile(path.resolve(__dirname, 'public', 'dist','index.html'));
 }) 
+app.use(cors({
+    origin: 'https://localhost:5173', // Replace with your frontend domain
+    credentials: true,
+  }));
 
 
 module.exports=app;
